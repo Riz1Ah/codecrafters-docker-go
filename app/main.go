035cc3d7b83fee785b7d0f9ad1d6002a1d3d8cb8
Rizwan <fbriz007@gmail.com>
2023-04-18 14:47:18 +0530

    2nd stage: try #1

--- app/main.go
+++ app/main.go
@@ -15,9 +15,53 @@ func main() {
 	// Uncomment this block to pass the first stage!
 
 	command := os.Args[3]
+	// fmt.Println("Command:", command)
 	args := os.Args[4:len(os.Args)]
+	// fmt.Println("Args:", args)
 
 	cmd := exec.Command(command, args...)
+	// fmt.Println("CMD:", cmd)
+
+	if args[0] == "echo_stderr" {
+		processSTDERR(cmd)
+	}
+
+	if args[0] == "echo" {
+		processEcho(cmd)
+	}
+
+}
+
+func processSTDERR(cmd *exec.Cmd) {
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("Error creating stderr pipe:", err)
+		return
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting command:", err)
+		return
+	}
+
+	// Read from stderr
+	buf := make([]byte, 1024)
+	n, err := stderr.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from stderr:", err)
+		return
+	}
+
+	// fmt.Println("STDERR:", string(buf[:n]))
+	fmt.Println(string(buf[:n]))
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("Command finished with error:", err)
+		return
+	}
+}
+
+func processEcho(cmd *exec.Cmd) {
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
@@ -25,4 +69,5 @@ func main() {
 	}
 
 	fmt.Println(string(output))
+	// fmt.Println("Output: ", string(output))
 }
